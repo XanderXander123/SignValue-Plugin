5397dcecdcdb9812390eaa66400c66fbf9b41eac
_x128 <143216072+XanderXander123@users.noreply.github.com>
2023-09-13 15:59:54 -0500

    Update SignValue.java

--- untitled4/src/main/resources/SignValue.java
+++ untitled4/src/main/resources/SignValue.java
@@ -14,35 +14,35 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public final class RgsPlugin extends JavaPlugin implements Listener {
   
-    @Override
+       @Override
        public void onEnable() {
-       getServer().getPluginManager().registerEvents(this, this);
+             getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        Action action = event.getAction();
-        if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
-            Block clickedBlock = event.getClickedBlock();
-            if (clickedBlock != null && isSign(clickedBlock)) {
-                Sign sign = (Sign) clickedBlock.getState();
-                if (sign.getLine(0).equals(ChatColor.DARK_RED + "[sell]")) {
-                    updateSignContents(sign, player);
-                    }
+          Player player = event.getPlayer();
+                Action action = event.getAction();
+                      if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
+                        Block clickedBlock = event.getClickedBlock();
+                             if (clickedBlock != null && isSign(clickedBlock)) {
+                               Sign sign = (Sign) clickedBlock.getState();
+                                   if (sign.getLine(0).equals(ChatColor.DARK_RED + "[sell]")) {
+                                     updateSignContents(sign, player);
+                                     }
             }
         }
     }
-    private boolean isSign(Block block) {
-        Material type = block.getType();
-                return type.equals(Material.SIGN) || type.equals(Material.SIGN_POST) || type.equals(Material.WALL_SIGN);
-    }
+    private boolean isSign(Block block,) {
+           Material type = block.getType();
+                   return type.equals(Material.SIGN) || type.equals(Material.SIGN_POST) || type.equals(Material.WALL_SIGN);
+                   }
 
     private void updateSignContents(Sign sign, Player player) {
-        sign.setLine(0, ChatColor.DARK_RED + "[sell]");
-            sign.setLine(1, player.getName());
-                sign.setLine(2, "0");
-                    sign.setLine(3, ChatColor.DARK_RED + "a");
-                        sign.update(); // Update the sign to reflect changes
-    }
+           sign.setLine(0, ChatColor.DARK_RED + "[sell]");
+               sign.setLine(1, player.getName());
+                   sign.setLine(2, "0");
+                       sign.setLine(3, ChatColor.DARK_RED + "a");
+                           sign.update(); // Update the sign to reflect changes
+                           }
 }
